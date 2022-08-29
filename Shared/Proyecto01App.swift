2c6983696b7cc1f05994eed9beaeda21c2b3c6bb
Ricardo Emiliano Apodaca Cardiel <emilianoa@ciencias.unam.mx>
2022-08-28 22:02:48 -0500

    Initial Commit

---
+++ Shared/Proyecto01App.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto01App.swift
+//  Shared
+//
+//  Created by Emiliano Apodaca on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
